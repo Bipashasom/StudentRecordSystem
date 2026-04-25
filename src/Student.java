76071561f0397a3cd99df6362cd2ec5b99c35ee2
Bipashasom <142660501+Bipashasom@users.noreply.github.com>
2026-04-26 01:38:58 +0530

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+        private int id;
+        private String name;
+        private int age;
+        private String grade;
+
+        public Student(int id, String name, int age, String grade) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.grade = grade;
+        }
+
+        public int getId() {
+          return id; 
+        }
+        public String getName() { 
+          return name; 
+        }
+        public int getAge() { 
+          return age; 
+        }
+        public String getGrade() { 
+          return grade; 
+        }
+        public void setGrade(String grade) { 
+          this.grade = grade; 
+        }
+
+        @Override
+        public String toString() {
+            return "ID: " + id + " | Name: " + name + " | Age: " + age + " | Grade: " + grade;
+        }
+    }
